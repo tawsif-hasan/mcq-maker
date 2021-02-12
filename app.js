e330b1dcd8d89e6ec79ad254c7a0531969d8969e
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-02-13 02:43:54 +0600

    Question Information Form Created. Form Fields: No Of Questions, Name of Institution, Total Marks, Total Time

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
-    res.send("<h1>MCQ Creator Application");
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/question_info.html", function(req,res){
+    res.sendFile(__dirname + "/question_info.html");
 })
 
+app.post("/question_info.html",function(req,res){
+    var noOfQuestions = Number(req.body.noOfQuestions);
+    var institutionName = req.body.institutionName;
+    var totalMarks = Number(req.body.totalMarks);
+    var totalTime = Number(req.body.totalTime);
+    console.log(noOfQuestions);
+    console.log(institutionName);
+    console.log(totalMarks);
+    console.log(totalTime);
+})
 
 app.listen(3000,function(){
     console.log("Server is running at port 3000");
