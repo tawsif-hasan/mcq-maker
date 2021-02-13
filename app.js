85c3062151c85d52def1c62b38305a8ad5cb46bd
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-02-13 17:37:38 +0600

    Question Information Completed. Questions and options can be seen in the webpage.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
@@ -17,12 +18,16 @@ app.post("/question_info.html",function(req,res){
     var institutionName = req.body.institutionName;
     var totalMarks = Number(req.body.totalMarks);
     var totalTime = Number(req.body.totalTime);
-    console.log(noOfQuestions);
-    console.log(institutionName);
-    console.log(totalMarks);
-    console.log(totalTime);
+    res.render("question", {noOfQuestions: noOfQuestions});
 })
 
+app.post("/question",function(req,res){
+    res.render("question-print", {question: req.body.question, 
+                                  optionA: req.body.optionA, 
+                                  optionB: req.body.optionB, 
+                                  optionC: req.body.optionC, 
+                                  optionD: req.body.optionD})
+})
 app.listen(3000,function(){
     console.log("Server is running at port 3000");
 })
