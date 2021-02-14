e5f112def4e7acb372b724990a2d5bfb94803ca6
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-02-14 15:35:56 +0600

    Version 1 complete.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+var pdf = require('html-pdf'); 
+
+let options = { 
+                format: 'A4',
+                border: {
+                    "top": "0.5in",            // default is 0, units: mm, cm, in, px
+                    "right": "1.5in",
+                    "bottom": "1.5in",
+                    "left": "1.5in"
+                  },
+                
+            };
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
@@ -18,16 +33,34 @@ app.post("/question_info.html",function(req,res){
     var institutionName = req.body.institutionName;
     var totalMarks = Number(req.body.totalMarks);
     var totalTime = Number(req.body.totalTime);
-    res.render("question", {noOfQuestions: noOfQuestions});
+    res.render("question", {noOfQuestions: noOfQuestions, institutionName: req.body.institutionName,
+                            totalMarks: req.body.totalMarks,
+                            totalTime:req.body.totalTime});
+})
+app.post('/question',(req,res)=>{
+    res.render('question-print',{
+                                noOfQuestions: Number(req.body.noOfQuestions),
+                                institutionName: req.body.institutionName,
+                                totalMarks: req.body.totalMarks,
+                                totalTime:req.body.totalTime,
+                                question: req.body.question, 
+                                optionA: req.body.optionA, 
+                                optionB: req.body.optionB, 
+                                optionC: req.body.optionC, 
+                                optionD: req.body.optionD},function(err,html){
+        pdf.create(html, options).toFile('./public/uploads/question.pdf', function(err, result) {
+            if (err){
+                return console.log(err);
+            }
+             else{
+            var datafile = fs.readFileSync('./public/uploads/question.pdf');
+            res.header('content-type','application/pdf');
+            res.send(datafile);
+             }
+          });
+    })
 })
 
-app.post("/question",function(req,res){
-    res.render("question-print", {question: req.body.question, 
-                                  optionA: req.body.optionA, 
-                                  optionB: req.body.optionB, 
-                                  optionC: req.body.optionC, 
-                                  optionD: req.body.optionD})
-})
 app.listen(3000,function(){
     console.log("Server is running at port 3000");
 })
