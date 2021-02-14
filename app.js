b6bb4c713f1d93a76f84407988e9f582dc56c2e5
Mohammad Tawsif Hasan <tawsifhasan@iut-dhaka.edu>
2021-02-14 16:37:19 +0600

    Start script added

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.post('/question',(req,res)=>{
     })
 })
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running at port 3000");
 })
